a067a25eb5beac69898f672829cd0c415fb426c4
Christian Dreier <flipper.x3r@googlemail.com>
2017-12-14 01:43:41 +0100

    Use meaningful color names

--- main.py
+++ main.py
@@ -34,14 +34,15 @@ HISTORY_LABEL_POS = 2, 132
 HISTORY_BORDER_SIZE = 10
 HISTORY_LINE_COUNT = 13
 
-WHITE      = 255, 255, 255
-LIGHTGREY  = 155, 155, 155
-DARKGREY   = 50 , 50 , 50 
-BLACK      = 0  , 0  , 0
-RED        = 255, 50 , 50
-YELLOW     = 255, 255, 55
-LIGHTGREEN = 50 , 255, 50
-DARKGREEN  = 50 , 200, 50
+AREA_LABEL_COLOR       = 155, 155, 155
+STATUS_AREA_BGCOLOR    = 50 , 50 , 50
+STATUS_LABEL_COLOR     = 255, 255, 255
+STATUS_INFO_FONT_COLOR = 0  , 0  , 0
+STATUS_INFO_BGCOLOR    = 255, 255, 55
+ONSWITCH_BGCOLOR       = 50 , 255, 50
+OFFSWITCH_BGCOLOR      = 255, 50 , 50
+HISTORY_AREA_BGCOLOR   = 0  , 0  , 0
+HISTORY_FONT_COLOR     = 50 , 200, 50
 
 LOOP_PAUSE_TIME = 10  # ms
 
@@ -57,27 +58,60 @@ class Status:
         self.update()
 
     def update(self):
-        self.window.fill(DARKGREY, STATUS_AREA_RECT)
-        self.window.blit(_font.render('Status Area', 1, LIGHTGREY, DARKGREY), STATUS_AREA_LABEL_POS)
+        self.window.fill(STATUS_AREA_BGCOLOR, STATUS_AREA_RECT)
+        self.window.blit(
+            _font.render('Status Area', 1, AREA_LABEL_COLOR, STATUS_AREA_BGCOLOR),
+            STATUS_AREA_LABEL_POS
+        )
 
-        pos = showtext(self.window, MOUSE_FOCUS_LABEL_POS, 'Mouse Focus', WHITE, DARKGREY)
+        pos = showtext(
+            self.window,
+            MOUSE_FOCUS_LABEL_POS,
+            'Mouse Focus',
+            STATUS_LABEL_COLOR,
+            STATUS_AREA_BGCOLOR
+        )
         self.window.blit(_switch_img[pygame.mouse.get_focused()], pos)
 
-        pos = showtext(self.window, KEYBOARD_FOCUS_LABEL_POS, 'Keyboard Focus', WHITE, DARKGREY)
+        pos = showtext(
+            self.window,
+            KEYBOARD_FOCUS_LABEL_POS,
+            'Keyboard Focus',
+            STATUS_LABEL_COLOR,
+            STATUS_AREA_BGCOLOR
+        )
         self.window.blit(_switch_img[pygame.key.get_focused()], pos)
 
-        pos = showtext(self.window, MOUSE_POSITION_LABEL_POS, 'Mouse Position', WHITE, DARKGREY)
+        pos = showtext(
+            self.window,
+            MOUSE_POSITION_LABEL_POS,
+            'Mouse Position',
+            STATUS_LABEL_COLOR,
+            STATUS_AREA_BGCOLOR
+        )
         mousepos = "{}, {}".format(*pygame.mouse.get_pos())
-        showtext(self.window, pos, mousepos, DARKGREY, YELLOW)
+        showtext(self.window, pos, mousepos, STATUS_INFO_FONT_COLOR, STATUS_INFO_BGCOLOR)
 
-        pos = showtext(self.window, LAST_KEYPRESS_LABEL_POS, 'Last Keypress', WHITE, DARKGREY)
+        pos = showtext(
+            self.window,
+            LAST_KEYPRESS_LABEL_POS,
+            'Last Keypress',
+            STATUS_LABEL_COLOR,
+            STATUS_AREA_BGCOLOR
+        )
         if self.lastkey:
             lastkey = f"{self.lastkey}, {pygame.key.name(self.lastkey)}"
         else:
             lastkey = 'None'
-        showtext(self.window, pos, lastkey, DARKGREY, YELLOW)
+        showtext(self.window, pos, lastkey, STATUS_INFO_FONT_COLOR, STATUS_INFO_BGCOLOR)
 
-        pos = showtext(self.window, INPUT_GRABBED_LABEL_POS, 'Input Grabbed', WHITE, DARKGREY)
+        pos = showtext(
+            self.window,
+            INPUT_GRABBED_LABEL_POS,
+            'Input Grabbed',
+            STATUS_LABEL_COLOR,
+            STATUS_AREA_BGCOLOR
+        )
         self.window.blit(_switch_img[pygame.event.get_grab()], pos)
 
 
@@ -89,7 +123,7 @@ def showtext(win, pos, text, color, bgcolor):
 
 def drawhistory(win, history):
     fontheight = _font.get_height()
-    win.blit(_font.render('Event History Area', True, LIGHTGREY, BLACK), HISTORY_LABEL_POS)
+    win.blit(_font.render('Event History Area', True, AREA_LABEL_COLOR, HISTORY_AREA_BGCOLOR), HISTORY_LABEL_POS)
     ypos = WINDOW_HEIGHT - HISTORY_BORDER_SIZE - fontheight
     h = list(history)
     h.reverse()
@@ -114,8 +148,8 @@ def main():
 
     _font = pygame.font.Font(None, FONT_SIZE)
 
-    _switch_img.append(_font.render("Off", True, BLACK, RED))
-    _switch_img.append(_font.render("On", True, BLACK, LIGHTGREEN))
+    _switch_img.append(_font.render("Off", True, STATUS_INFO_FONT_COLOR, OFFSWITCH_BGCOLOR))
+    _switch_img.append(_font.render("On", True, STATUS_INFO_FONT_COLOR, ONSWITCH_BGCOLOR))
 
     history = []
     status = Status(win)
@@ -125,10 +159,10 @@ def main():
         joystick = pygame.joystick.Joystick(i)
         joystick.init()
         txt = f'Enabled joystick: {joystick.get_name()}'
-        img = _font.render(txt, True, DARKGREEN, BLACK)
+        img = _font.render(txt, True, HISTORY_FONT_COLOR, HISTORY_AREA_BGCOLOR)
         history.append(img)
     if pygame.joystick.get_count() == 0:
-        img = _font.render('No Joysticks to Initialize', True, DARKGREEN, BLACK)
+        img = _font.render('No Joysticks to Initialize', True, HISTORY_FONT_COLOR, HISTORY_AREA_BGCOLOR)
         history.append(img)
 
     running = True
@@ -153,10 +187,10 @@ def main():
 
             if e.type != pygame.MOUSEMOTION:
                 txt = f"{pygame.event.event_name(e.type)}: {e.dict}"
-                img = _font.render(txt, True, DARKGREEN, BLACK)
+                img = _font.render(txt, True, HISTORY_FONT_COLOR, HISTORY_AREA_BGCOLOR)
                 history.append(img)
                 history = history[-HISTORY_LINE_COUNT:]
-                
+
         status.update()
         drawhistory(win, history)
 
